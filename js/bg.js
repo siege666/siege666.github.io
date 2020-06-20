2c4493d94ce64feaf4463d95f904602ef76573a8
siege666 <67174539+siege666@users.noreply.github.com>
2020-06-20 10:45:39 +0900

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -1,11 +1,11 @@
 const UNSPLASH_URL = "https://source.unsplash.com"
 const body = document.queryseletor("body");
-function paintBg()(
-    body.style.backgroundImage = `url(SUNPLASH_URL))`;
-)
+function paintBg(){
+    body.style.backgroundImage = `url(${UNPLASH_URL})`;
+}
 
-function init()(
+function init(){
   paintBg();
-)
+}
 
 init();
