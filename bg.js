cd6cba9a2e74f14c36d1f19b62af26deb8d62683
siege666 <67174539+siege666@users.noreply.github.com>
2020-06-20 10:30:40 +0900

    Create bg.js

---
+++ bg.js
@@ -0,0 +1,11 @@
+const UNSPLASH_URL = "https://source.unsplash.com"
+const body = document.queryseletor("body");
+function paintBg()(
+    body.style.backgroundImage = `url(SUNPLASH_URL))`;
+)
+
+function init()(
+  paintBg();
+)
+
+init();
